c5502c211ed3471fd1c7f5906a833665870b87c7
Crumbler <25861574+Crumbler@users.noreply.github.com>
2024-08-16 10:45:16 +0200

    Add shutdown

--- FabulousDuster/MovementManager.cs
+++ FabulousDuster/MovementManager.cs
@@ -2,12 +2,15 @@
 using FabulousDuster.Helpers;
 using FabulousDuster.Wrappers;
 
+using System.Globalization;
+
 namespace FabulousDuster;
 
 public static class MovementManager {
     private static bool _isMoving,
         _isShuttingDown;
-    private static CancellationTokenSource? _movementCancelSource;
+    private static CancellationTokenSource? _movementCancelSource,
+        _shutdownCancelSource;
 
     public static void AddMovementHotKeys() {
         HotKeyManager.AddHotKey(KeyModifiers.Control | KeyModifiers.Alt, Keys.B, OnMovementHotkey);
@@ -69,13 +72,73 @@ public static class MovementManager {
     }
 
     private static void OnShutdownHotkey() {
-        if (_isShuttingDown) {
-            Console.WriteLine("Cannot start movement while shutting down");
+        if (_isMoving) {
+            Console.WriteLine("Cannot start shutdown while moving");
             return;
         }
 
         _isShuttingDown = !_isShuttingDown;
 
+        if (_isShuttingDown) {
+            _shutdownCancelSource?.Dispose();
+            _shutdownCancelSource = new CancellationTokenSource();
+            ShutdownFunc(_shutdownCancelSource.Token).ContinueWith((t, args) => {
+                if (t.IsCanceled) {
+                    Console.WriteLine("Shutdown canceled");
+                }
+            }, TaskContinuationOptions.OnlyOnCanceled, TaskScheduler.Default);
+        } else {
+            _shutdownCancelSource?.Cancel();
+        }
+
         Console.WriteLine("Shutdown " + (_isShuttingDown ? "enabled" : "disabled"));
+    }
+
+    private static async Task ShutdownFunc(CancellationToken cancellationToken) {
+        if (!CursorWrapper.GetCursorPos(out POINT cursorPos)) {
+            throw new Exception("Failed to get cursor position");
+        }
+
+        Console.WriteLine("Please enter the shutdown hour");
+        int hour = int.Parse(Console.ReadLine()!, CultureInfo.InvariantCulture);
+
+        Console.WriteLine("Please enter the shutdown minute");
+        int minute = int.Parse(Console.ReadLine()!, CultureInfo.InvariantCulture);
+
+        Console.WriteLine("Shutdown based off of cursor position: " + cursorPos);
+
+        POINT leftPos = cursorPos with {
+            X = cursorPos.X - 50,
+            Y = cursorPos.Y - 80
+        },
+        rightPos = cursorPos with {
+            X = cursorPos.X + 50,
+            Y = cursorPos.Y - 80
+        };
+
+        do {
+            await Task.Delay(5000, cancellationToken);
+            cancellationToken.ThrowIfCancellationRequested();
+
+            MouseHelper.MoveToPointAndClick(leftPos);
+
+            await Task.Delay(5000, cancellationToken);
+            cancellationToken.ThrowIfCancellationRequested();
+
+            MouseHelper.MoveToPointAndClick(rightPos);
+
+            var timeNow = DateTime.Now;
+
+            if (timeNow.Hour >= hour && timeNow.Minute > minute) {
+                _isShuttingDown = false;
+                MouseHelper.MoveToPointAndClick(cursorPos);
+
+                Console.WriteLine("Shutting down due to time");
+                return;
+            }
+        }
+        while (!cancellationToken.IsCancellationRequested);
+
+        cancellationToken.ThrowIfCancellationRequested();
     }
 }
